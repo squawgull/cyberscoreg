d38487f94f9543aaa299c7908c12eb6f3185888c
squawgull <94125811+squawgull@users.noreply.github.com>
2023-04-16 22:34:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def proceed():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
     if final == True:
+        print("Process is terminating")
         sys.exit()
 
 #procedures needed
@@ -237,10 +238,5 @@ def main(inp):
     userScore = calculate(bowRep)
     locate(processedData)
     
-    display()
-    
-    print(userScore)
-    print(flaggedPosts)
-    print(termsFound)
-    
+    display()   
     clean()

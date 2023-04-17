dadb65ab714ba7511d0f3bc993a3ce554b058f95
squawgull <94125811+squawgull@users.noreply.github.com>
2023-04-17 12:27:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -104,6 +104,7 @@ def exit():
     del locations
     del termsFound
     del terms
+    return render_template('home.html')
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)

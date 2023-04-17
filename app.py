fced5531c9d96b9ea3fa1ca3b48d4ea0202c04e1
squawgull <94125811+squawgull@users.noreply.github.com>
2023-04-17 11:53:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,12 @@ def clause():
 
 @app.route('/proceed') #displays results
 def proceed():
-    return render_template('proceed.html', score = userScore, len = len(flaggedPosts), flaggedPosts = flaggedPosts), shut()
+    return render_template('proceed.html', score = userScore, len = len(flaggedPosts), flaggedPosts = flaggedPosts)
+
+@app.route('/finish')
+def finish():
+    shut()
+    
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
